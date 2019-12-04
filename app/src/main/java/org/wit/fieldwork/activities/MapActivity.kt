6fa2811c3d413540367e68f2f11d9a9a5efd9135
Mark Murphy <markmurphydunhill@gmail.com>
2019-12-04 10:59:51 +0000

    map

--- app/src/main/java/org/wit/fieldwork/activities/MapActivity.kt
+++ app/src/main/java/org/wit/fieldwork/activities/MapActivity.kt
@@ -1,5 +1,50 @@
 package org.wit.fieldwork.activities
 
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.Marker
+import org.wit.fieldwork.R
+
+class MapActivity : AppCompatActivity(), GoogleMap.OnMarkerDragListener, GoogleMap.OnMarkerClickListener {
+
+    lateinit var map: GoogleMap
+    lateinit var presenter: MapPresenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_edit_location)
+        setContentView(R.layout.activity_map)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        presenter = MapPresenter(this)
+        mapFragment.getMapAsync {
+            map = it
+            map.setOnMarkerDragListener(this)
+            map.setOnMarkerClickListener(this)
+            presenter.initMap(map)
+        }
+    }
+
+    override fun onMarkerDragStart(marker: Marker) {}
+
+    override fun onMarkerDrag(marker: Marker) {}
+
+    override fun onMarkerDragEnd(marker: Marker) {
+        presenter.doUpdateLocation(marker.position.latitude, marker.position.longitude, map.cameraPosition.zoom)
+    }
+
+    override fun onBackPressed() {
+        presenter.doOnBackPressed()
+    }
+
+    override fun onMarkerClick(marker: Marker): Boolean {
+        presenter.doUpdateMarker(marker)
+        return false
+    }
+}
+/*
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
@@ -68,4 +113,4 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerD
         marker.setSnippet("GPS : " + loc.toString())
         return false
     }
-}
+}*/

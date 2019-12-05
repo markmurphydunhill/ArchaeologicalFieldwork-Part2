b5e7a880cc44f95a10b6dfdc8abea0bdcee80515
Mark Murphy <markmurphydunhill@gmail.com>
2019-12-05 14:07:25 +0000

    Base Presenter for map

--- app/src/main/java/org/wit/fieldwork/views/map/FieldworkMapsPresenter.kt
+++ app/src/main/java/org/wit/fieldwork/views/map/FieldworkMapsPresenter.kt
@@ -1,5 +1,37 @@
 package org.wit.fieldwork.views.map
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import org.wit.fieldwork.models.FieldworkModel
+import org.wit.fieldwork.views.BasePresenter
+import org.wit.fieldwork.views.BaseView
 
+class FieldworkMapsPresenter(view: BaseView) : BasePresenter(view) {
+
+    fun doPopulateMap(map: GoogleMap, fieldworks: List<FieldworkModel>) {
+        map.uiSettings.setZoomControlsEnabled(true)
+        fieldworks.forEach {
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.title).position(loc)
+            map.addMarker(options).tag = it.id
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+        }
+    }
+
+    fun doMarkerSelected(marker: Marker) {
+        val tag = marker.tag as Long
+        val fieldwork = app.fieldworks.findById(tag)
+        if (fieldwork != null) view?.showFieldwork(fieldwork)
+
+    }
+
+    fun loadFieldworks() {
+        view?.showFieldworks(app.fieldworks.findAll())
+    }
+}
+/*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
@@ -31,4 +63,4 @@ class FieldworkMapsPresenter(val view: FieldworkMapsView) {
         val fieldwork = app.fieldworks.findById(tag)
         if (fieldwork != null) view.showFieldwork(fieldwork)
     }
-}
+}*/
--- app/src/main/java/org/wit/fieldwork/views/map/FieldworkMapsView.kt
+++ app/src/main/java/org/wit/fieldwork/views/map/FieldworkMapsView.kt
@@ -1,6 +1,77 @@
 package org.wit.fieldwork.views.map
 
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.Marker
+import org.wit.fieldwork.R
+import kotlinx.android.synthetic.main.activity_fieldwork_maps.*
+import kotlinx.android.synthetic.main.content_fieldwork_maps.*
+import org.wit.fieldwork.helpers.readImageFromPath
+import org.wit.fieldwork.models.FieldworkModel
+import org.wit.fieldwork.views.BaseView
+
+class FieldworkMapsView : BaseView(), GoogleMap.OnMarkerClickListener {
+
+    lateinit var presenter: FieldworkMapsPresenter
+    lateinit var map : GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fieldwork_maps)
+        super.init(toolbar)
+
+        presenter = initPresenter (FieldworkMapsPresenter(this)) as FieldworkMapsPresenter
+
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync {
+            map = it
+            map.setOnMarkerClickListener(this)
+            presenter.loadFieldworks()
+        }
+    }
+
+    override fun showFieldwork(fieldwork: FieldworkModel) {
+        currentTitle.text = fieldwork.title
+        currentDescription.text = fieldwork.description
+        currentImage.setImageBitmap(readImageFromPath(this, fieldwork.image1))
+    }
+
+    override fun showFieldworks(fieldworks: List<FieldworkModel>) {
+        presenter.doPopulateMap(map, fieldworks)
+    }
+
+    override fun onMarkerClick(marker: Marker): Boolean {
+        presenter.doMarkerSelected(marker)
+        return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+}
+/*
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.Marker
@@ -65,7 +136,7 @@ class FieldworkMapsView : AppCompatActivity(), GoogleMap.OnMarkerClickListener {
         mapView.onSaveInstanceState(outState)
     }
 }
-/*
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -141,4 +212,5 @@ class FieldworkMapsView : AppCompatActivity(), GoogleMap.OnMarkerClickListener {
         super.onSaveInstanceState(outState)
         mapView.onSaveInstanceState(outState)
     }
-}*/
+}
+*/
